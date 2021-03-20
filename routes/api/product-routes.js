ea5a29a29b1fa7400a9afdf5657a534226dde0fe
ian mcbrayer <mcbrayer.ian@gmail.com>
2021-03-20 12:31:14 -0400

    fixed typo at line 18 of product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
       res.status(404).json({ message: 'No products found!' });
       return;
     }
-    res.statur(200).json(productData);
+    res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
   }
